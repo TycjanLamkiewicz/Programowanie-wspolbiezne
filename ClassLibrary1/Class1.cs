ae6ad5e6ba828a8ed816e791cefcd3b5eb163277
Tycjan Lamkiewicz 247716 <247716@edu.p.lodz.pl>
2024-03-12 20:37:09 +0100

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Bilard
+    {
+        public int add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
---
+++ ClassLibrary1/SuperBilard.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class SuperBilard : Bilard
+    {
+
+    }
+    public class SuperBilard2 : Bilard
+    {
+
+    }
+}
